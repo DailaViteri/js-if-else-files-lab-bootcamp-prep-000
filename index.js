d009517862762b636f6257ffc95ec3874d4cb847
Daila Viteri <dailaviteri@gmail.com>
2018-03-07 17:39:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ if (name === "Susan") {
 if (height === "70") {
     console.log("The height is correct")
 } else {
-    console.log("Expected the name to be Susan")
+    console.log("Expected the height to be 70")
 }
