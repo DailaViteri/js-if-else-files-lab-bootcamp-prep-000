4baa216e9e9fd35af75f4f1db73afbfc8e473035
Daila Viteri <dailaviteri@gmail.com>
2018-03-07 17:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (name === "Susan") {
     console.log("Expected the name to be Susan")
 }
 if (height === "70") {
-    console.log("The name is correct")
+    console.log("The height is correct")
 } else {
     console.log("Expected the name to be Susan")
 }
